9b1b8bcccc37084b9f5823195302a7ff4886417e
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-09-30 20:43:22 +0100

    building button

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { withExpoSnack } from 'nativewind';
+import { StyleSheet, Text, View,Image,Alert, SafeAreaView, Button } from 'react-native';
+// import { withExpoSnack } from 'nativewind';
 
 export default function App() {
+  let state = false;
+
+  const pressed =() => {
+    // state =!state
+    Alert.alert("Today's Motivation", "Life hard, Give up",[
+      {text: "Yes"},
+      {text: "Yes, I will"}
+    ])
+  }
   return (
-    <View style={styles.container}>
-      <Text>welcome to Expo</Text>
+    <SafeAreaView style={styles.container}>
+      
+      {/* <Text 
+        numberOfLines={1} 
+        style={styles.text}
+          >welcome to Expo welcome to Expo
+        </Text> */}
+
+
+      <Button 
+        title='Click Me' 
+        style= {styles.text}
+        onPress={pressed}>Click Me</Button>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    textColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#fff',
     justifyContent: 'center',
+    alignItems:'center'
   },
+  text: {
+    color: 'yellow',
+    fontSize:'20px',
+    fontWeight: '600',
+  }
 });
