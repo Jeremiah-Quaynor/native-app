6096388c0a13926779a26d04722d7af345d8142a
Jeremiah-Quaynor <jeremiah.quaynor@stu.ucc.edu.gh>
2022-11-04 22:49:07 +0000

    adding styles

--- screens/Explorer.jsx
+++ screens/Explorer.jsx
@@ -1,12 +1,21 @@
-import { View, Text } from 'react-native'
+import { View, Text , StyleSheet} from 'react-native'
 import React from 'react'
 
 const Explorer = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Explorer</Text>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    color:  'white'
+  }
+})
 
 export default Explorer
